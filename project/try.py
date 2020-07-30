9244bb351ce0ee6a478a098aafcdc127f0acba95
“baokai98 <1781700743@qq.com>
2020-07-30 17:12:46 +0800

    python-game

--- project/try.py
+++ project/try.py
@@ -1,18 +1,18 @@
 import random
 # 先定义一个函数
-def fight():
+while True:
     # 定义四个变量：分别为我的血量、我的攻击力、你的血量、你的攻击力
 
     my_hp = 1000
     my_pow = 200
     you_hp = 1000
     you_pow = 200
-    pow_qx = random.randint(0,1)
+    pow_qx = random.randint(1,2)
 
     # 为了实现多次对打，使用while循环
-    while True:
+
         # 对打一次后我的血量=我原来的血量-你打我一次的攻击力
-        if pow_qx = 0:
+        if pow_qx = 1:
             my_hp = my_hp - you_pow
         # 对打一次后你的血量=你原来的血量-我打你一次的攻击力
             you_hp = you_hp - my_pow
@@ -30,4 +30,3 @@ def fight():
             # 终止对打，跳出循环
             break
 # 调用定义的函数
-fight()

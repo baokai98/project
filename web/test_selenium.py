9b460865890bd07907afa79abe7c6188bf8521a6
“baokai98 <1781700743@qq.com>
2020-08-23 23:29:41 +0800

    python-game

---
+++ web/test_selenium.py
@@ -0,0 +1,11 @@
+from time import sleep
+
+import selenium
+from selenium import webdriver
+from selenium import webdriver
+
+
+def test_selenium():
+    driver = webdriver.Chrome()
+    driver.get('https://hao.360.com/')
+    sleep(10)
